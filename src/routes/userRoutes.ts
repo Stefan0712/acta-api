69ad51de773198926bd804d4e3e66971249579ce
Stefan0712 <s.vladulescu@gmail.com>
2025-12-12 09:56:55 +0000

    Created User model, controllers for searching a user and updating a profile, and setup user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+const express = require('express');
 import { updateProfile, searchUsers } from '../controllers/userController';
 import { protect } from '../middleware/authMiddleware';
 
